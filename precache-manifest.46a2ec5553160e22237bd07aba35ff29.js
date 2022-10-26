19d31be1b6a2c71e2d2e3c40ef16bb1d404ffe45
Elvis <elviscruz45@gmail.com>
2022-10-25 21:23:05 -0500

    Updates

---
+++ precache-manifest.46a2ec5553160e22237bd07aba35ff29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4e48abe3aa827f09507",
+    "url": "/react-js-2/static/css/main.39b74821.chunk.css"
+  },
+  {
+    "revision": "a4e48abe3aa827f09507",
+    "url": "/react-js-2/static/js/main.a4e48abe.chunk.js"
+  },
+  {
+    "revision": "37520b7e5b5c16f1b14c",
+    "url": "/react-js-2/static/js/1.37520b7e.chunk.js"
+  },
+  {
+    "revision": "d279d22297aaa44b0a5e",
+    "url": "/react-js-2/static/js/runtime~main.d279d222.js"
+  },
+  {
+    "revision": "a6ff2ce648ff3421426db7842d40d1d0",
+    "url": "/react-js-2/static/media/check.a6ff2ce6.svg"
+  },
+  {
+    "revision": "45d0d71a1f8f92c1da6744303a373163",
+    "url": "/react-js-2/static/media/delete.45d0d71a.svg"
+  },
+  {
+    "revision": "1dd01b7226dc516715ddd577e73572ae",
+    "url": "/react-js-2/index.html"
+  }
+];
